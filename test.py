d549e22daf8b37662b1d169e4be9e35d67511d07
m-caetano <m.caetano@usp.br>
2023-06-06 17:29:27 +0200

    Remove test.py

--- test.py
+++
@@ -1,9 +0,0 @@
-## This is just a test to see if pushing works.
-
-from parapy.geom import Box
-from parapy.gui import display
-
-obj = Box(1, 2, 3)
-display(obj)
-
-# Testing if I can also commit and push.
