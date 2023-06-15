c07a16444686bd3437fe1bd92f50e86fa3b84ef0
m-caetano <m.caetano@usp.br>
2023-06-15 11:44:27 +0200

    sparsystem updated to include spar_stations attribute. wingbox updated to remove the same attribute.

--- wingbox.py
+++ wingbox.py
@@ -2,7 +2,6 @@ from parapy.core import *
 from parapy.geom import *
 from ribssystem import RibsSystem
 from sparsystem import SparSystem
-import numpy as np
 from skinsystem import SkinSystem
 from winggeom import WingGeom
 
@@ -25,44 +24,14 @@ class WingBox(GeomBase):
     TE_ribs_gap = Input(0.8)  # Must be after the rearmost rear_spar_loc but less than 1
     TE_skin_gap = Input(0.85)  # Must be after the rearmost rear_spar_loc but less than 1
 
-    @Attribute
-    def spar_stations(self):
-
-        frac_span = np.array(self.wing.spans) / self.wing.spans[-1]
-        air_sec = self.wing.airfoil_sections
-        air_sec = air_sec[1:-1]
-
-        front_loc = self.front_spar_loc
-        rear_loc = self.rear_spar_loc
-        front_loop = []
-        rear_loop = []
-
-        for i in air_sec:
-            frac_span = np.append(frac_span, i)
-            order_sp = np.sort(frac_span)
-            order = np.argsort(frac_span)
-            pos = np.where(order_sp == i)[0][0]
-            front_loc.append(0)
-            rear_loc.append(0)
-            front_loop = list([front_loc[k] for k in order])
-            rear_loop = list([rear_loc[k] for k in order])
-            front_loop[pos] = front_loop[pos - 1] + (i-order_sp[pos-1]) \
-                              * (front_loop[pos+1] - front_loop[pos-1])/(order_sp[pos+1] - order_sp[pos-1])
-            rear_loop[pos] = rear_loop[pos - 1] + (i-order_sp[pos-1]) \
-                             * (rear_loop[pos+1] - rear_loop[pos-1])/(order_sp[pos+1] - order_sp[pos-1])
-            front_loc[-1] = front_loop[pos]
-            rear_loc[-1] = rear_loop[pos]
-
-        return front_loop, rear_loop
-
     @Part
     def skin(self):
         return SkinSystem(TE_gap=self.TE_skin_gap)
 
     @Part
     def spars(self):
-        return SparSystem(front_spar_loc=self.spar_stations[0],
-                          rear_spar_loc=self.spar_stations[1])
+        return SparSystem(front_spar_loc=self.front_spar_loc,
+                          rear_spar_loc=self.rear_spar_loc)
 
     @Part
     def ribs(self):
